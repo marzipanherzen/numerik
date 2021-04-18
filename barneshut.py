5f32dde079bbe286ef5a7b80d36da1b773ec4b8a
Victoria <herbig.victoria@gmail.com>
2021-04-18 18:26:13 +0100

    combined velocity and vorticity inducing instead of seperate functions

--- barneshut.py
+++ barneshut.py
@@ -14,6 +14,8 @@ created 16/04/2021 by @V. Herbig
 import math
 import numpy as np
 from numba import njit, prange
+import time
+import xlsxwriter
 
 alpha = 0.5
 
@@ -33,6 +35,17 @@ class Branch:
         self.GLeaf = None
 
 def insertPoint(x,y,G,branch):
+    '''
+    Insert a vortex into the tree using the following recursive procedure
+    i) if branch does not contain a vortex, put vortex as leaf here
+    ii) if branch is a child (internal branch), update the center-of-vorticity and total vorticity.
+        recursively insert the vortex in the appropriate quadrant
+    iii) if branch is a leaf (external branch), then there are two vortices in the same region. 
+        subdivide the region further by creating children. then, recursively insert both vortices into the appropriate quadrant(s).
+        since both vortices may still end up in the same quadrant, there may be several subdivisions during
+        a single insertion.
+        Finally, update the center-of-vorticity and total vorticity of branch
+    '''
     if not branch.hasChild and not branch.hasLeaf:
         branch.xLeaf = x
         branch.yLeaf = y
@@ -83,53 +96,60 @@ def insertPoint(x,y,G,branch):
         branch.yLeaf += y*G # same as for x 
         branch.GLeaf += G # total vorticity increases
 
-def calc_vel(x1,x2,y1,y2,G2,h):
+def calculations(x1,x2,y1,y2,G1,G2,h,visco,multi_flag=True,gammas_flag=True):
+    '''
+    Calculate velocity and vorticity induced from one vortice one the other \\
+    Vortice 1 is the one on which velocity is induced \\
+    If a multipole is considered (multi_flag == True), the velocity is approximated via "Multipolentwicklung"
+        x2 is then equivalent to \Gamma_i x_i, i.e. the weighted sum over all x in that branch
+        y2 analog
+        G is then equivalent to \Gamma_i 1_i, i.e. the sum over all gammas in that branch
+    If relation of free and boundary vortices is considered no vorticity is exchanged (gammas_flag == False)
+    '''
     dx = x1 - x2
     dy = y1 - y2
     d2 = dx**2 + dy**2 + h
+
     U = (G2/(2*np.pi)) * (dy/d2)
     V = (G2/(2*np.pi)) * (dx/d2)
 
-    return U, V
+    if multi_flag:
+        U -= x2 * (1/(2*np.pi)) * (1/(d2**2)) * 2*dx*dy
+        V -= y2 * (1/(2*np.pi)) * (1/(d2**2)) * (-2)*dx*dy
 
-# @njit(fastmath=True, parallel=True)
-def barnes_velocity(x,y,h,G,branch,alpha,U=0.0,V=0.0):
+    w = None
+    if gammas_flag:
+        w = (G2-G1) * h**2 * visco * 4 * (5*d2 - 2*h)/((d2)**4)
+
+    return U, V, w
+
+def barnes_exchange(x,y,h,G,branch,visco,alpha,U=0.0,V=0.0,W=0.0):
     if branch.hasLeaf:
-        if not branch.xLeaf == x and not branch.yLeaf == y and not branch.GLeaf == G:
-            u, v = calc_vel(x,branch.xLeaf,y,branch.yLeaf,branch.GLeaf,h)
+        if not branch.xLeaf == x and not branch.yLeaf == y:
+            u, v, w = calculations(x,branch.xLeaf,y,branch.yLeaf,G,branch.GLeaf,h,visco,multi_flag=False,gammas_flag=True)
             U -= u
             V += v
+            W += w
     else:
         xm = branch.xLeaf / branch.GLeaf # center of branch is given by (x1*g1 + x2*g2 + ...)/G where G = g1 + g2 + ...
         ym = branch.yLeaf / branch.GLeaf # same as for x
         dist = math.sqrt( ((x-xm)**2)+((y-ym)**2) )
-        if branch.D/dist < alpha:
-            u, v = calc_vel(x,xm,y,ym,branch.GLeaf,h)
+        if branch.D/(dist+h) < alpha: # the h is added to prevent division by 0 in case vortice and weighted center of branch are very close
+            u, v, w = calculations(x,xm,y,ym,G,branch.GLeaf,h,visco,multi_flag=True,gammas_flag=True)
             U -= u
-            V += v            
+            V += v
+            W += w            
         else:
-            for child in branch.children: # maybe implement this as > for i in prange(len(branch.children)) < to make use of njit parallel
+            for child in branch.children: 
                 if child is not None:
-                    vel = barnes_velocity(x,y,h,G,child,alpha,U,V)
+                    vel, w = barnes_exchange(x,y,h,G,child,visco,alpha,U,V,W)
                     U -= vel.real
                     V += vel.imag
+                    W += w
 
-    return U + 1j*V
+    return U + 1j*V, w
 
-def barnes_gammas(elements, h, gammas, visco):
-    N = len(elements)
-    w = np.zeros((N), dtype = np.float64)
-
-    for i in prange(N):
-        for k in prange(N):
-            dxik = (elements[i].real - elements[k].real)**2
-            dyik = (elements[i].imag - elements[k].imag)**2
-            d2ik = dxik + dyik
-            w[i] += (gammas[k]-gammas[i]) * h**2 * visco * 4 * (5*d2ik - h)/((d2ik + h)**4)
-    return w
-
-# @njit(fastmath=True, parallel=True)
-def barnes_collatz(vortices, bd_vortices, vel_inf, dt, h, gammas, bd_gammas, N, visco, x_d, y_d):
+def barnes_collatz(vortices, bd_vortices, vel_inf, dt, h, gammas, bd_gammas, N, visco, alpha, x_d, y_d):
     # first collatz step
     # create initial tree with center in the middle of domain 
     xm_root = x_d[0] + math.sqrt( ((x_d[0]-x_d[1])**2) ) / 2
@@ -138,18 +158,21 @@ def barnes_collatz(vortices, bd_vortices, vel_inf, dt, h, gammas, bd_gammas, N, 
     root_half = Branch(xm_root,ym_root,D_root)
 
     # insert all free vortices
+    # tic = time.time()
     for i in prange(len(vortices)):
         insertPoint(vortices[i].real,vortices[i].imag,gammas[i],root_half)
+    # toc = time.time()
+    # runtime = toc-tic
+    # print("Time to insert {} particles in octree is {}s." .format(len(vortices), toc-tic))
 
     # half step
     u_half = np.zeros((len(vortices)), dtype = np.complex128)
+    w_gamma_half = np.zeros((len(vortices)), dtype = np.float64)
     for j in prange(len(vortices)):
-        u_half[j] = barnes_velocity(vortices[j].real,vortices[j].imag,h,gammas[j],root_half,alpha)
+        u_half[j], w_gamma_half[j] = barnes_exchange(vortices[j].real,vortices[j].imag,h,gammas[j],root_half,visco,alpha)
         for k in prange(len(bd_vortices)):
-            U, V = calc_vel(vortices[j].real,bd_vortices[k].real,vortices[j].imag,bd_vortices[k].imag,bd_gammas[k],h)
+            U, V, w = calculations(vortices[j].real,bd_vortices[k].real,vortices[j].imag,bd_vortices[k].imag,gammas[j],bd_gammas[k],h,visco,multi_flag=False,gammas_flag=False)
             u_half[j] += U + 1j*V
-
-    w_gamma_half = barnes_gammas(vortices,h,gammas,visco)
 
     mid_points = vortices + (u_half + vel_inf)*0.5*dt
     mid_gammas = gammas + w_gamma_half*0.5*dt
@@ -162,16 +185,71 @@ def barnes_collatz(vortices, bd_vortices, vel_inf, dt, h, gammas, bd_gammas, N, 
         insertPoint(mid_points[i].real,mid_points[i].imag,gammas[i],root_full)
 
     u_full = np.zeros((len(mid_points)), dtype = np.complex128)
+    w_gamma_full = np.zeros((len(vortices)), dtype = np.float64)
     for j in prange(len(mid_points)):
-        u_full[j] = barnes_velocity(mid_points[j].real,mid_points[j].imag,h,gammas[j],root_full,alpha)
+        u_full[j], w_gamma_full[j] = barnes_exchange(mid_points[j].real,mid_points[j].imag,h,gammas[j],root_full,visco,alpha)
         for k in prange(len(bd_vortices)):
-            U, V = calc_vel(mid_points[j].real,bd_vortices[k].real,mid_points[j].imag,bd_vortices[k].imag,bd_gammas[k],h)
+            U, V, w = calculations(mid_points[j].real,bd_vortices[k].real,mid_points[j].imag,bd_vortices[k].imag,mid_gammas[j],bd_gammas[k],h,visco,multi_flag=False,gammas_flag=False)
             u_full[j] += U + 1j*V
 
-    w_gamma = barnes_gammas(mid_points,h,mid_gammas,visco)
-
     vortices += (u_full + vel_inf)*dt
-    gammas += w_gamma*dt
+    gammas += w_gamma_full*dt
 
-    return vortices, gammas
+    return vortices, gammas #, runtime
 
+##### PAST VERSIONS #####
+# def calc_vel(x1,x2,y1,y2,G2,h,multi_flag=True):
+    '''
+    Calculate velocity induced from one vortice one the other
+    Vortice 1 is the one on which velocity is induced
+    If a multipole is considered (multi_flag == True), the velocity is approximated via "Multipolentwicklung"
+        x2 is then equivalent to \Gamma_i x_i, i.e. the weighted sum over all x in that branch
+        y2 analog
+        G is then equivalent to \Gamma_i 1_i, i.e. the sum over all gammas in that branch
+    '''
+    dx = x1 - x2
+    dy = y1 - y2
+    d2 = dx**2 + dy**2 + h
+    U = (G2/(2*np.pi)) * (dy/d2)
+    V = (G2/(2*np.pi)) * (dx/d2)
+
+    if multi_flag:
+        U -= x2 * (1/(2*np.pi)) * (1/(d2**2)) * 2*dx*dy
+        V -= y2 * (1/(2*np.pi)) * (1/(d2**2)) * (-2)*dx*dy
+
+    return U, V
+
+# def barnes_velocity(x,y,h,G,branch,alpha,U=0.0,V=0.0):
+    if branch.hasLeaf:
+        if not branch.xLeaf == x and not branch.yLeaf == y:
+            u, v = calc_vel(x,branch.xLeaf,y,branch.yLeaf,branch.GLeaf,h,multi_flag=False)
+            U -= u
+            V += v
+    else:
+        xm = branch.xLeaf / branch.GLeaf # center of branch is given by (x1*g1 + x2*g2 + ...)/G where G = g1 + g2 + ...
+        ym = branch.yLeaf / branch.GLeaf # same as for x
+        dist = math.sqrt( ((x-xm)**2)+((y-ym)**2) )
+        if branch.D/(dist+h) < alpha: # the h is added to prevent division by 0 in case vortice and weighted center of branch are very close
+            u, v = calc_vel(x,xm,y,ym,branch.GLeaf,h)
+            U -= u
+            V += v            
+        else:
+            for child in branch.children: 
+                if child is not None:
+                    vel = barnes_velocity(x,y,h,G,child,alpha,U,V)
+                    U -= vel.real
+                    V += vel.imag
+
+    return U + 1j*V
+
+# def barnes_gammas(elements, h, gammas, visco):
+    N = len(elements)
+    w = np.zeros((N), dtype = np.float64)
+
+    for i in prange(N):
+        for k in prange(N):
+            dxik = (elements[i].real - elements[k].real)**2
+            dyik = (elements[i].imag - elements[k].imag)**2
+            d2ik = dxik + dyik
+            w[i] += (gammas[k]-gammas[i]) * h**2 * visco * 4 * (5*d2ik - h)/((d2ik + h)**4)
+    return w
